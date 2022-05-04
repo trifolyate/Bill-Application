0140a9b7f8e7e2511c6e7ca94e41e9fef8d33b5d
trifolyate <21008946@myrp.edu.sg>
2022-05-04 23:50:41 +0800

    Added extra features and some more on the aesthetic side. The reset button is a textview. Will try to do a pop up message for the next update rather than just a text appearing at the bottom of the screen.

--- app/src/main/java/sg/edu/rp/c346/id21008946/billplease/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21008946/billplease/MainActivity.java
@@ -3,12 +3,14 @@ package sg.edu.rp.c346.id21008946.billplease;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -47,30 +49,50 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                double total_amount = Integer.parseInt(AmountFields.getText().toString());
-                double pax = Integer.parseInt(PaxFields.getText().toString());
-                double discount = Integer.parseInt(DiscountFields.getText().toString());
-                double final_amount = total_amount -(total_amount * (discount/100));
-                if(SVStb.isChecked())
+                if(!TextUtils.isEmpty(AmountFields.getText().toString()))
                 {
-                    final_amount = final_amount * 1.1;
-                }
-                if(GSTtb.isChecked())
-                {
-                    final_amount = final_amount * 1.07;
-                }
-                double split_amount = final_amount/pax;
-                if(Cashrb.isChecked())
-                {
-                    String result = String.format("Total Bill: $%.2f\nEach Pays: $%.2f in cash",final_amount,split_amount);
-                    ResultView.setText(result);
+                    if(!TextUtils.isEmpty(PaxFields.getText().toString()))
+                    {
+                        if(!TextUtils.isEmpty(DiscountFields.getText().toString()))
+                        {
+                            double total_amount = Integer.parseInt(AmountFields.getText().toString());
+                            double pax = Integer.parseInt(PaxFields.getText().toString());
+                            double discount = Integer.parseInt(DiscountFields.getText().toString());
+                            double final_amount = total_amount -(total_amount * (discount/100));
+                            if(SVStb.isChecked())
+                            {
+                                final_amount = final_amount * 1.1;
+                            }
+                            if(GSTtb.isChecked())
+                            {
+                                final_amount = final_amount * 1.07;
+                            }
+                            double split_amount = final_amount/pax;
+                            if(Cashrb.isChecked())
+                            {
+                                String result = String.format("Total Bill: $%.2f\nEach Pays: $%.2f in cash",final_amount,split_amount);
+                                ResultView.setText(result);
+                            }
+                            else
+                            {
+                                String result = String.format("Total Bill: $%.2f\nEach Pays: $%.2f via PayNow to 91111111",final_amount,split_amount);
+                                ResultView.setText(result);
+                            }
+                        }
+                        else
+                        {
+                            Toast.makeText(MainActivity.this,"Please Enter Discount percentage (Discount)",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else
+                    {
+                        Toast.makeText(MainActivity.this,"Please Enter Number of People(Pax)",Toast.LENGTH_SHORT).show();
+                    }
                 }
                 else
                 {
-                    String result = String.format("Total Bill: $%.2f\nEach Pays: $%.2f via PayNow to 91111111",final_amount,split_amount);
-                    ResultView.setText(result);
+                    Toast.makeText(MainActivity.this,"Please Enter the Amount of the Bill(Amount)",Toast.LENGTH_SHORT).show();
                 }
-                
             }
         });
 
@@ -84,6 +106,7 @@ public class MainActivity extends AppCompatActivity {
                 DiscountFields.setText("");
                 Cashrb.setChecked(true);
                 Paynowrb.setChecked(false);
+                ResultView.setText("");
             }
         });
     }
